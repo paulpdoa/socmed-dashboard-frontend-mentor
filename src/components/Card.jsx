984a8fd79f36561ca6fa07c22b48b662d55e5a8b
Paul Andres <polopdoandres@gmail.com>
2022-07-03 11:24:16 +0800

    cannot change the color for instagram icons

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,10 @@
 
 
-const Card = ({ customCard,user,followers,today,icon,arrow,toggle }) => {
+const Card = ({ customCard,user,followers,today,icon,arrow,toggle,cardIconColor }) => {
   return (
     <div className={toggle ? `card__dark ${customCard}` : `card__light ${customCard}`}>
         <div className="card__body">
-            <span className="card__account">{icon} {user}</span>
+            <span className="card__account"><span className={icon.type.name === 'FaFacebook' ? 'facebook' : icon.type.name === 'FaInstagram' ? 'instagram' : icon.type.name === 'FaYoutube' ? 'youtube' : 'twitter' }>{icon}</span> {user}</span>
             <h1 className={toggle ? `card__number__light` : `card__number__dark`}>{followers}</h1>
             <span className="card__follower">FOLLOWERS</span>
             <span className="card__today">{arrow} {today} Today</span>
